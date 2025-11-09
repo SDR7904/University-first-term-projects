5ec43a985711c93c8c316f308802fa5e23a8c1a4
yasafan890 <m.sadra.kouchakzadeh@gmail.com>
2025-11-09 20:02:23 +0330

    debug the code

--- exercises/E2/exercise-2.py
+++ exercises/E2/exercise-2.py
@@ -1,8 +1,10 @@
 time = int(input())
 # hours, minutes, seconds = 0
+days = time // 86400
+time %= 86400
 hours = time // 3600
-time = time % 3600
+time %= 3600
 minutes = time // 60
-time = time % 60
+time %= 60
 seconds = time
 print(f"{hours}:{minutes}:{seconds}")
