c294833e003728fc864bebd6d0d700795cda3d06
yasafan890 <m.sadra.kouchakzadeh@gmail.com>
2025-11-11 17:07:50 +0330

    Create Two-Jobs.py

---
+++ exercises/E4/Two-Jobs.py
@@ -0,0 +1,10 @@
+even = ["shanbe", "doshanbe", "chaharshanbe"]
+odd = ["yekshanbe", "seshanbe", "panjshanbe"]
+weekend = ["jome"]
+day = input()
+if day in even:
+    print("perspolis")
+elif day in odd:
+    print("bahman")
+else:
+    print("tatil")
